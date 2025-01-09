eaf184d2ca43ead44b752ef8701c49383c57c419
Shivam Kumar Jha <shkrjh@gmail.com>
2025-01-09 11:33:45 +0530

    findAge file added

---
+++ findAge.java
@@ -0,0 +1,10 @@
+public class findAge {
+	public static void main(String[] args) {
+		String name = "Harry";
+		int birthYear = 2000;
+		int currentYear = 2024;
+		int currentAge = currentYear - birthYear;
+
+		System.out.println(name + " age is " + currentAge);
+	}
+}
